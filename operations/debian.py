062f9b18456857a3fd82b04c5b4212869c3796ba
Mathieu Charron <mathieu@tx0.dev>
2025-05-18 22:35:06 -0400

    Fixed debian operations.

--- operations/debian.py
+++ operations/debian.py
@@ -1,3 +1,4 @@
+from io import StringIO
 from pyinfra.api.deploy import deploy
 from pyinfra.api.operation import operation
 from pyinfra.operations import apt, files
@@ -19,7 +20,24 @@ def set_timezone(zone: str = "America/Toronto"):
     )
 
 
-@deploy("Install tooling")
+@operation()
+def enable_sid():
+    """
+    Enable the sid repository
+    """
+    yield from apt.repo._inner(
+        src="deb http://ftp.ca.debian.org/debian sid main contrib", filename="sid"
+    )
+    yield from files.put._inner(
+        dest="/etc/apt/preferences.d/99-debian-sid",
+        src=StringIO("""Package: *
+Pin: release a=unstable
+Pin-Priority: 10k"""),
+    )
+    yield from apt.update._inner()
+
+
+@deploy("Tooling")
 def install_tooling():
     _ = apt.packages(
         name="Install tools",
